0bcce136f7ad3b9a1a3dc766e4a1a622806ab201
Eniola Osabiya <eosabiya@gmail.com>
2021-12-20 10:51:27 +0100

    Simple screens!

---
+++ pages/app.js
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <>
+            <section class="bg-white h-screen">
+                <div class="flex flex-col h-screen overflow-hidden bg-gray-900 pattern">
+                    <div class="container flex items-center flex-1 px-6 py-8 mx-auto lg:py-0">
+                        <div class="max-w-3xl mx-auto text-center text-white">
+                            <h1 class="text-2xl font-semibold leading-relaxed text-white lg:text-5xl">
+                                In Progres....
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default App;
